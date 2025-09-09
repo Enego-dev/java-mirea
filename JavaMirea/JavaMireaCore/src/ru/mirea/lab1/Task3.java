c31a6cb1735626c0ee28af3c2088c539bf7d7572
Enego-dev <themrxxxyou@gmail.com>
2025-09-09 23:21:47 +0300

    коммент обязательно

--- JavaMirea/JavaMireaCore/src/ru/mirea/lab1/Task3.java
+++ JavaMirea/JavaMireaCore/src/ru/mirea/lab1/Task3.java
@@ -2,6 +2,7 @@ package ru.mirea.lab1;
 
 public class Task3 {
     public static void main(String[] args){
+        // добавляем аргументы: Edit Configurations -> Arguments
         for (String arg : args){
             System.out.println(arg);
         }
