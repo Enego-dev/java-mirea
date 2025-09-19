38e6fb8a7ca2607c360d079addb91b15a9a6d566
Enego-dev <themrxxxyou@gmail.com>
2025-09-19 23:30:42 +0300

    second task

---
+++ JavaMirea/JavaMireaCore/src/ru/mirea/lab2/author/AuthorTest.java
@@ -0,0 +1,11 @@
+package ru.mirea.lab2.author;
+
+public class AuthorTest {
+    public static void main(String[] args){
+        // ctrl p || ctrl q для показа сигнатуры метода в IntelliJ IDEA
+        var author = new Author("Медведев Михаил Евгеньевич", "medvedev.m.e@edu.mirea.ru", 'M');
+        System.out.println(author.toString());
+        author.setEmail("medvedev.m.e.test.email@edu.mirea.ru");
+        System.out.println(author.toString());
+    }
+}
